79b00a61371f02b1f6f5a993d4f42c2310dc30f8
CimimUxMaio <cimimuxmaio@gmail.com>
2022-09-15 17:05:51 -0300

    Replace custom HttpError with FastAPI's HTTPException for automatic error handling

--- src/errors.py
+++ src/errors.py
@@ -1,22 +1,20 @@
+from fastapi import HTTPException
 from http.client import BAD_REQUEST, NOT_FOUND
 
 
-class HttpError(Exception):
-    def __init__(self, code: int, msg: str):
-        self._code = code
-        super().__init__(msg)
-
-
-class FilePathNotFound(HttpError):
+class FilePathNotFound(HTTPException):
     def __init__(self, file_path: str):
-        super().__init__(NOT_FOUND, f"The given file path: {file_path} was not found.")
+        super().__init__(status_code=NOT_FOUND,
+                         detail=f"The given file path: {file_path} was not found.")
 
 
-class JSONIDNotFound(HttpError):
+class JSONIDNotFound(HTTPException):
     def __init__(self, id: int):
-        super().__init__(NOT_FOUND, f"No json file was found for the given id: {id}")
+        super().__init__(status_code=NOT_FOUND,
+                         detail=f"No json file was found for the given id: {id}")
 
 
-class JSONFileAlreadyOpen(HttpError):
+class JSONFileAlreadyOpen(HTTPException):
     def __init__(self, file_path: str):
-        super().__init__(BAD_REQUEST, f"The given Dataset JSON file at: {file_path} is already open.")
+        super().__init__(status_code=BAD_REQUEST,
+                         detail=f"The given Dataset JSON file at: {file_path} is already open.")
