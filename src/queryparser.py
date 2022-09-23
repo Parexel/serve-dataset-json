3d2445eeea4d66037d867c04f85771ea28fcf1ea
Osipov <Ivan.Osipov@parexel.com>
2022-09-23 16:52:40 -0300

    Initial queryparser

--- src/queryparser.py
+++ src/queryparser.py
@@ -2,19 +2,15 @@
 def parse(filter: str, varList: list):
 
     len_varList = len(varList)
+    n = len_varList
     adaptetd_var_list = []
     for i in varList:
         adaptetd_var_list.append(i.lower())
 
     def resultParse(row: list):
-        len_rowList = len(row)
-        if len_rowList != len_varList:
-            return 0
-        else:
-            n = len_rowList
 
         for i in range(n):
             exec(adaptetd_var_list[i] + " = row[i]")
-        return eval(filter)
+        return bool(eval(filter))
 
     return resultParse
